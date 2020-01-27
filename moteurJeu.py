57f941e9a50a84490bfdec19f29daa0d351acc80
Jeremy LAVEILLE <jeremy.laveille@edu.esiee.fr>
2020-01-27 20:33:43 +0100

    Fonctionnement du gagnant sur les diagonales + Tests

--- moteurJeu.py
+++ moteurJeu.py
@@ -3,9 +3,13 @@ import numpy as np
 
 class MoteurJeu:
     def __init__(self):
-        self.joueur = Joueur()
-        self.ia = IA()
+        #self.joueur = Joueur()
+        #self.ia = IA()
         self.grille = Grille()
+
+    def Placer(self,jeton):
+        self.grille.grille[jeton.x][jeton.y] = jeton.idJoueur
+        print("Jeton placé en (",jeton.x,",",jeton.y,")")
     
     def Gagnant(self, jeton):
         compteur = 0
@@ -17,7 +21,7 @@ class MoteurJeu:
                 compteur = 0
                 break
             if compteur == 5:
-                return idJoueur
+                return jeton.idJoueur
         compteur = 0
         # Vérificaion ligne
         for elmL in range(self.grille.largeur):
@@ -26,17 +30,46 @@ class MoteurJeu:
             else:
                 compteur = 0
             if compteur == 5:
-                return idJoueur
-        #Vérification diagonale
-
+                return jeton.idJoueur
+        compteur = 0
+        # Vérification diagonale 1
+        dirX = 1
+        dirY = 1
+        for elmD1 in range(self.grille.hauteur):
+            case = self.grille.grille[jeton.x + elmD1*dirX][jeton.y + elmD1*dirY]
+            if(case == jeton.idJoueur):
+                compteur += 1           #Tant que notre case est à l'id du joueur on augmente le compteur
+            else:
+                dirX = -1               #Si on a une case différente on change de direction de la diagonale
+                dirY = -1
+            if(compteur == 5):
+                return jeton.idJoueur   #Si le compteur est a 5 le joueur id gagne
+            elif(dirX == -1 and dirY == -1 and case != jeton.idJoueur):
+                compteur = 0            #Si on a déjà changer de direction et qu'il y a une case différente le joueur ne gagne pas
+                break
+        compteur = 0
+        dirX = 1
+        dirY = -1
+        for elmD2 in range(self.grille.hauteur):
+            case = self.grille.grille[jeton.x + elmD2*dirX][jeton.y + elmD2*dirY]
+            if(case == jeton.idJoueur):
+                compteur += 1           #Tant que notre case est à l'id du joueur on augmente le compteur
+            else:
+                dirX = -1               #Si on a une case différente on change de direction de la diagonale
+                dirY = 1
+            if(compteur == 5):
+                return jeton.idJoueur   #Si le compteur est a 5 le joueur id gagne
+            elif(dirX == -1 and dirY == 1 and case != jeton.idJoueur):
+                compteur = 0            #Si on a déjà changer de direction et qu'il y a une case différente le joueur ne gagne pas
+                break
 
 class Grille:
     def __init__(self):
-        self.grille = [[0,0,0,0,0,0,0,0,-1],
+        self.grille = [[0,0,0,0,0,0,0,1,-1],
+                       [0,0,0,0,0,0,0,1,-1],
                        [0,0,0,0,0,0,0,0,-1],
-                       [0,0,0,0,0,0,0,0,-1],
-                       [0,0,0,0,0,0,0,0,-1],
-                       [0,0,0,0,0,0,0,0,-1],
+                       [0,0,0,0,0,0,0,1,-1],
+                       [0,0,0,0,0,0,0,1,-1],
                        [0,0,0,0,0,0,0,0,-1],
                        [0,0,0,0,0,0,0,0,-1],
                        [0,0,0,0,0,0,0,0,-1],
@@ -53,7 +86,20 @@ class Grille:
                     break
         return casesVides
 
+class Jeton: 
+    #Classe Jeton très temporaire juste pour tester le bon fonctionnement de MoteurJeu
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.idJoueur = 1
 
+
+#Test des classes et de leurs méthodes
 if __name__ == "__main__":
     Grille1 = Grille()
-    print(Grille1.CasesVides())
+    Moteur1 = MoteurJeu()
+    Jeton1 = Jeton(2,7)
+
+    print("Coordonnées des cases vides: ",Grille1.CasesVides())
+    Moteur1.Placer(Jeton1)
+    print("Id du Joueur gagnant: ",Moteur1.Gagnant(Jeton1))
