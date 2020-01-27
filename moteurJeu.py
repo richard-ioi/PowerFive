a36061a859f5d67c8766f7c47a189b5f998392df
Abdelmalik KERBADOU <abdelmalik.kerbadou@edu.esiee.fr>
2020-01-27 17:16:21 +0100

    Création classe MoteurJeu

--- moteurJeu.py
+++ moteurJeu.py
@@ -1,6 +1,35 @@
 import pygame
 import numpy as np
 
+class MoteurJeu:
+    def __init__(self):
+        self.joueur = Joueur()
+        self.ia = IA()
+        self.grille = Grille()
+    
+    def Gagnant(self, jeton):
+        compteur = 0
+        # Vérification colonne
+        for elmC in range(jeton.y, self.grille.hauteur, 1):
+            if( self.grille.grille[jeton.x][elmC] == jeton.idJoueur ):
+                compteur += 1
+            else:
+                compteur = 0
+                break
+            if compteur == 5:
+                return idJoueur
+        compteur = 0
+        # Vérificaion ligne
+        for elmL in range(self.grille.largeur):
+            if( self.grille.grille[elmL][jeton.y] == jeton.idJoueur ):
+                compteur += 1
+            else:
+                compteur = 0
+            if compteur == 5:
+                return idJoueur
+        #Vérification diagonale
+
+
 class Grille:
     def __init__(self):
         self.grille = [[0,0,0,0,0,0,0,0,-1],
@@ -17,10 +46,10 @@ class Grille:
 
     def CasesVides(self):
         casesVides = []
-        for ligne in range(len(self.grille)):
+        for colonne in range(len(self.grille)):
             for elm in range(self.hauteur, -1, -1):
-                if(self.grille[ligne][elm] == 0):
-                    casesVides.append( (ligne, elm) )
+                if(self.grille[colonne][elm] == 0):
+                    casesVides.append( (colonne, elm) )
                     break
         return casesVides
 
