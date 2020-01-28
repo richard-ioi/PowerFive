65b10c38eb0c0eb55df13d3aeaffb0af681ca684
Abdelmalik KERBADOU <abdelmalik.kerbadou@edu.esiee.fr>
2020-01-28 11:44:15 +0100

    Gestion du temps et du placement du pion)

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import pygame
 from moteurJeu import *
 from Joueur import Joueur
 
+pygame.init()
+Clock = pygame.time.Clock()
+
 GrilleDeJeu = Grille()
-MoteurDeJeu = MoteurJeu(GrilleDeJeu)
+MoteurDeJeu = MoteurJeu(GrilleDeJeu, Clock)
 JoueurH = Joueur()
 #JetonActuel = None
 
-pygame.init()
-
 while True:
+    Clock.tick()
     event = pygame.event.Event(pygame.USEREVENT)
     pygame.event.pump()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             break
-
+    
     posSouris = JoueurH.CliqueSouris(event, MOUSEBUTTONDOWN)
     MoteurDeJeu.Placer(posSouris)
