d40beae9c60f3dc492e296b7368329769a23e342
richard-ioi <richard.fouquoire@edu.esiee.fr>
2020-02-03 14:07:24 +0100

    MAJ avec ajout main POO, Richard

--- main.py
+++ main.py
@@ -1,84 +1,75 @@
 #! /usr/bin/env python3
 
-"""
-    Fichier principal initialisant l'ensemble des classes du jeu.
-"""
 import pygame
 import os
-from moteurJeu import *
+import sys
+from controleur import *
 from modeles import *
 from vue import *
 
-pygame.init()
-Clock = pygame.time.Clock()
+"""
+    Fichier principal initialisant l'ensemble des classes du jeu.
+"""
 
-test = False
+class Main:
 
-largeurFenetre = 1280
-hauteurFenetre = 720
-Fenetre = pygame.display.set_mode( (largeurFenetre, hauteurFenetre) )
-pygame.display.set_caption("PowerFive")
-
-music_pinguTheme = os.path.join("data","sons","pingu_theme.wav")
-pygame.mixer.music.load(music_pinguTheme)
-pygame.mixer.music.play(loops=-1,start=0.0)
-
-AnimBase = { "Sheriff": Animation(Fenetre, os.path.join("sheriff", "char.png"), 0, 42, 7, True, 62, 64),
-             "Froggy": Animation(Fenetre, os.path.join("froggy", "char.png"), 0, 24, 5),
-             "Jurassy": Animation(Fenetre, os.path.join("jurassy", "char.png"), 0, 13, 5, True, 62, 72),
-             "Pingu": Animation(Fenetre, os.path.join("pingu","char.png"), 0,27,5,True,62,96),
-             "Weasel": Animation(Fenetre, os.path.join("weasel","char.png"),0,13,7,True,62,72),
-             "PinguBad": Animation(Fenetre, os.path.join("pingu","char_bad.png"), 0,33,5,True,62,96) }
-
-AnimSpec = { "test":Animation(Fenetre, os.path.join("sheriff", "char.png"), 0, 42, 7, True, 62, 64) }
-
-AnimBoutonUlti = [ Animation(Fenetre, os.path.join("ultimate","ultimate1.png"),0,1,1,True,56,47,None,3,True),
-                   Animation(Fenetre, os.path.join("ultimate","ultimateboucle0.png"),0,12,2,False,56,47),
-                   Animation(Fenetre, os.path.join("ultimate","ultimateboucle.png"),0,12,3,True,56,47) ]
-
-GrilleDeJeu = Grille()
-InterfaceJeu = Interface(Fenetre, largeurFenetre, hauteurFenetre, GrilleDeJeu, AnimBase, AnimSpec)
-MoteurDeJeu = MoteurJeu(InterfaceJeu, GrilleDeJeu, Clock)
-
-idJoueur = 0
-
-posSouris = (0,0)
-
-boutonUlti = ObjetAnimMultiple(85,240,AnimBoutonUlti,AnimBase)   #Creation du bouton d'ultime à partir de la classe Bouton
-conditionsAnimBouton = [False,False,False]
-print(GrilleDeJeu)
-
-while True:
-    Clock.tick(60)
-    FPS = Clock.get_fps()
-    #print(FPS)
-    #print(boutonUlti.currentAnim.play)
-    if InterfaceJeu.compteur==0:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                
-                if( boutonUlti.currentAnim.rect.collidepoint(posSouris) ):
-                    boutonUlti.clicked = True
-
-                for rect in InterfaceJeu.rectColonne:
-                    if rect["rect"].collidepoint( (posSouris[0]-5, posSouris[1]) ):
-                        MoteurDeJeu.Placer(rect["colonne"],idJoueur)
-                        boutonUlti.Reinitialiser()
-                        print(GrilleDeJeu)
-
-    if(InterfaceJeu.tourJoueur): idJoueur = 1
-    else: idJoueur = 2
-    posSouris = pygame.mouse.get_pos()
-    #Affichage
-    boutonUlti.updateCurrentAnim(condition=boutonUlti.clicked)
-    InterfaceJeu.Affichage()
-    InterfaceJeu.AttentePlacement(posSouris,idJoueur)
+    def __init__(self):
+        self.initJeu()
     
-    #screen.blit(imageJeton,(screenLargeur//2-imageJeton.get_width()//2-4 , posJeton))
+    def initJeu(self):
+        pygame.init()
+        self.Clock = pygame.time.Clock()
+        self.fps = 60
+        self.largeur = 1280
+        self.hauteur = 720
+        self.titre = "PowerFive"
+        self.fenetre = pygame.display.set_mode( (self.largeur, self.hauteur) )
+        """self.music = Jukebox( musics = { "Pingu": os.path.join("data", "musiques", "pingu_theme.wav",
+                                         "Devil Pingu": os.path.join("data", "musiques", "devil_pingu_theme.wav" },
+                              sounds = { "Explosion": pygame.mixer.Sound( os.path.join("data", "sons", "explosion.wav") ),
+                                         "Succes": pygame.mixer.Sound( os.path.join("data", "sons", "succes.wav") ) } )
+        self.music.playMusic("Pingu")"""
+        self.animBase = { "Sheriff": Animation(self.fenetre, os.path.join("sheriff", "char.png"), 0, 42, 7, True, 62, 64),
+                          "Froggy": Animation(self.fenetre, os.path.join("froggy", "char.png"), 0, 24, 5),
+                          "Weasel": Animation(self.fenetre, os.path.join("weasel", "char.png"), 0, 13, 5, True, 62, 72),
+                          "Jurassy": Animation(self.fenetre, os.path.join("jurassy", "char.png"), 0, 13, 5, True, 62, 72),
+                          "Pingu": Animation(self.fenetre, os.path.join("pingu","char.png"), 0,27,5,True,62,96),
+                          "PinguBad": Animation(self.fenetre, os.path.join("pingu","char_bad.png"), 0,33,5,True,62,96) }
+        self.animSpec = {}
+        self.grille = Grille()
+        self.interface = Interface(self.fenetre, self.largeur, self.hauteur, self.grille, self.animBase, self.animSpec)
+        self.moteur = MoteurJeu(self.interface, self.grille, self.Clock)
+        self.ia = IA(self.moteur)
     
-    if(InterfaceJeu.dialogueFini==False):
-        InterfaceJeu.afficheTexte(2,"Bonjour je m'appelle Pingu")
-    pygame.display.update()
+    def mainLoop(self):
+        while True:
+            self.Clock.tick(self.fps)
+            pygame.display.set_caption(self.titre)
+
+            posSouris = pygame.mouse.get_pos()
+
+            #self.grille.CasesVides()
+
+            if self.interface.tourJoueur:
+                idJoueur = 1
+            else:
+                idJoueur = 2
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for rect in self.interface.rectColonne:
+                        if(self.interface.tourJoueur):
+                            if rect["rect"].collidepoint( (posSouris[0]-5, posSouris[1]) ):
+                                self.moteur.Placer(rect["colonne"], idJoueur)
+            if(not self.interface.tourJoueur):
+                self.ia.IAPlay()
+            self.interface.Affichage()
+            self.interface.AttentePlacement(posSouris, idJoueur)
+
+            pygame.display.update()
+
+if __name__ == "__main__":
+    jeu = Main().mainLoop()
