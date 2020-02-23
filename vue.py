64f8686ca50af8a4af877b440d48721281a75cb6
richard-ioi <richard.fouquoire@edu.esiee.fr>
2020-02-23 21:46:12 +0100

    Changement de la table + des ennemis froggy, jurassy et weasel

--- vue.py
+++ vue.py
@@ -68,7 +68,7 @@ class Interface:
         self.personnage = personnage
 
         self.background = scale(pygame.image.load(os.path.join("data","graphismes","saloon","saloon_clone.png")),(1278,720))
-        self.tableAnimation = Animation(self.fenetre, os.path.join("table_animation.png"),0,32,2,True,428,17,None,3,True,0,669)
+        self.tableAnimation = Animation(self.fenetre, os.path.join("table_animation.png"),0,32,4,True,428,17,None,3,True,0,669)
         #self.tableAnimation = Animation(self.fenetre, os.path.join("table_animation.png"),0,32,10,True,428,17,None,4,True,-213,631)
 
         self.scoreIA=0
